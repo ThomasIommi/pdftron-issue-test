a6b4d9961ad95d60e2b53f591fd868f2db00232a
Thomas Iommi <thomasiommi6r@gmail.com>
2022-10-18 23:12:20 +0200

    feat: sample of issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Core } from '@pdftron/webviewer';
+
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    Core: typeof Core;
+  }
+}
+
+export const PDFTRON = window.Core;
+PDFTRON.setWorkerPath('/lib/pdftron/core');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pdftron-issue-test';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('scrollView') scrollViewRef!: ElementRef<HTMLElement>;
+  @ViewChild('viewer') viewerRef!: ElementRef<HTMLElement>;
+
+  private viewer!: Core.DocumentViewer;
+
+  async ngAfterViewInit() {
+    this.viewer = new PDFTRON.DocumentViewer();
+    this.viewer.setViewerElement(this.viewerRef.nativeElement);
+    this.viewer.setScrollViewElement(this.scrollViewRef.nativeElement);
+    await this.viewer.loadDocument('/assets/PDFTRON_about.pdf')
+    const textSelectTool = this.viewer.getTool(PDFTRON.Tools.ToolNames.TEXT_SELECT);
+    this.viewer.setToolMode(textSelectTool);
+  }
+
 }
